37547edafdd66e7ccc0a46d9a0075a3a4b290e2c
ram kirge <kirgeram489@gmail.com>
2025-07-12 14:40:34 +0530

    complete  project structure

---
+++ frontend/rewear/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-10xl bg-amber-700 text-amber-200 '>nhkinjknj</h1>
+    </div>
+  )
+}
+
+export default App
